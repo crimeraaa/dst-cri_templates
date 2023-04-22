36e930431ce6207c1161ca189c91bc40257ee76d
crimeraaa <124979548+crimeraaa@users.noreply.github.com>
2023-04-22 20:23:42 +0800

    Update startserver.bat

--- startserver.bat
+++ startserver.bat
@@ -18,13 +18,21 @@ cd /D "%server%/bin"
 
 set cluster=<full path to your cluster folder>
 
+:: example: set cluster=C\Users\My Username\Documents\Klei\DoNotStarveTogether\Crimeraaas_Testing_Server
+
 set modsetup=<full path to 'dedicated_server_mods_setup.lua'>
 
+:: example: set modsetup=C\Users\My Username\Documents\Klei\DoNotStarveTogether\Dedicated Server Settings\dedicated_server_mods_setup.lua
+
 set modoverrides=<full path to 'modoverrides.lua'>
+
+:: example: set modoverrides=C\Users\My Username\Documents\Klei\DoNotStarveTogether\Crimeraaas_Testing_Server\modoverrides.lua
 
 :: remove the '/s' parameter below if you're trying to overwrite a file within the same parent folder.
 
 xcopy /s /i "%modsetup%" "%server%\mods" /y
+
+:: my modoverrides for my testing server is in the same parent folder, i'll have to omit the /s.
 
 xcopy /s /i "%modoverrides%" "%cluster%\Master\" /y
 
