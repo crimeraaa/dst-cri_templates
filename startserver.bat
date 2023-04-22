bfbdc025a64a162f4bcda35e1f3dfc7859a50772
crimeraaa <124979548+crimeraaa@users.noreply.github.com>
2023-04-22 20:24:10 +0800

    Update startserver.bat

--- startserver.bat
+++ startserver.bat
@@ -18,15 +18,15 @@ cd /D "%server%/bin"
 
 set cluster=<full path to your cluster folder>
 
-:: example: set cluster=C\Users\My Username\Documents\Klei\DoNotStarveTogether\Crimeraaas_Testing_Server
+:: example: set cluster=C:\Users\My Username\Documents\Klei\DoNotStarveTogether\Crimeraaas_Testing_Server
 
 set modsetup=<full path to 'dedicated_server_mods_setup.lua'>
 
-:: example: set modsetup=C\Users\My Username\Documents\Klei\DoNotStarveTogether\Dedicated Server Settings\dedicated_server_mods_setup.lua
+:: example: set modsetup=C:\Users\My Username\Documents\Klei\DoNotStarveTogether\Dedicated Server Settings\dedicated_server_mods_setup.lua
 
 set modoverrides=<full path to 'modoverrides.lua'>
 
-:: example: set modoverrides=C\Users\My Username\Documents\Klei\DoNotStarveTogether\Crimeraaas_Testing_Server\modoverrides.lua
+:: example: set modoverrides=C:\Users\My Username\Documents\Klei\DoNotStarveTogether\Crimeraaas_Testing_Server\modoverrides.lua
 
 :: remove the '/s' parameter below if you're trying to overwrite a file within the same parent folder.
 
