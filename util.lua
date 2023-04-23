8c7d058c6c15a52f4a3361b2a6c126ed85e3ec82
crimeraaa <124979548+crimeraaa@users.noreply.github.com>
2023-04-24 07:13:15 +0800

    Update util.lua

--- util.lua
+++ util.lua
@@ -1,55 +1,83 @@
 -- call modimport("path_from_modroot/util") and all the non-locals here will be ready to use.
 
--- [[ CONSTANTS ]]
-_G = GLOBAL 
+_G = GLOBAL -- :hmmmm_pray:
 
+-- ======================
 -- [[ C++ ENGINE STUFF ]]
-TheNet = _G.TheNet
-
+-- ======================
+TheNet = _G.TheNet 
+TheShard = _G.TheShard
+-- ===============
+-- [[ CONSTANTS ]]
+-- ===============
+TENDENCY = _G.TENDENCY
+-- ===================
 -- [[ GLOBAL TABLES ]]
+-- ===================
 STRINGS = _G.STRINGS
-
+-- =========================================
 -- [[ EMPTY BUT INITIALIZED GLOBAL TABLES ]]
--- these empty tables are initialized and will eventually be populated by the game.
+-- =========================================
+-- see main.lua
 AllPlayers = _G.AllPlayers
 Ents = _G.Ents
-
--- some tables aren't initialized at the time the mod is being compiled, such as ThePlayer, TheWorld, etc. see main.lua.
--- other tables like Prefabs can't be declared here, for some reason?
-
+--[[
+other tables like Prefabs can't be declared here, for some reason.
+some tables aren't initialized at the time the mod is being compiled, such as ThePlayer, TheWorld, etc.
+^ for those cases you'll have to declare them as global within functions you intend to call them in.
+]]
+-- ======================
 -- [[ GLOBAL FUNCTIONS ]]
+-- ======================
 SpawnPrefab = _G.SpawnPrefab
-debug = _G.debug
-unpack = _G.unpack -- not within mod env so gotta call global
-dumptable = _G.dumptable -- DST specific
-
+FirstToUpper = _G.FirstToUpper
+-- ==========
+-- [[ MISC ]]
+-- ==========
+debug = _G.debug 
+unpack = _G.unpack 
+dumptable = _G.dumptable 
+--[[
+debug is a Lua library, but Klei did not set it to be included within the mod env. So we initialize debug = _G.debug
+similar case with unpack.
+dumptable is a function Klei made themselves, it prints out all contents of a table including subtables, sub-subtables, until the very last.
+]]
+-- =========================
 -- [[ SHORTHAND FUNCTIONS ]]
-gsub = string.gsub
-stringf = string.format -- i use string.format a ton, so this seems reasonable?
-function printf(fmt, ...) -- stands for "Print Format".
-	print(stringf(fmt, ...)) 
+-- =========================
+gsub = string.gsub -- no other Lua fn uses the name "gsub" so i'll assume it's always related to strings.
+tinsert = table.insert 
+stringf = string.format 
+function printf(format, ...) -- stands for "Print Format".
+	print(stringf(format, ...)) 
 end 
-
--- [[ DEBUG FUNCTIONS ]]
--- tbh this is all i have LMAO
-
-local should_debug = true -- set this to false if you want the mod to shut up
-function printd(...)
-	-- initialize to do nothing
+-- =====================
+-- [[ DEBUG PRINTOUTS ]] 
+-- =====================
+prettyname = "My Mod Name: "
+function debugprint(msg, ...)
 end
 
-local prettyname = "[[DEBUG]] MY MOD NAME: "
-if should_debug then -- will pass if should_debug == true or anything that's *isn't* nil or false.
-	printd = function(fmt, ...)
-  		printf(prettyname..fmt, ...)
-	end
+-- you can create a simple configuration option, or just set the value.
+-- local should_debug = GetModConfigData("should_debug")
+local should_debug = true
+if should_debug then
+    local debugname = "[DEBUG] "..string.upper(prettyname)
+    debugprint = function(msg, ...) -- msg can be a stringf or a simple string
+        printf(debugname..msg, ... ) 
+    end
 end
 
--- ==================================
--- [[ EXAMPLE UTIL FUNCTIONS I USE ]]
--- ==================================
+local cmd_msg_name = prettyname.."%s"
+function cmdprintf(msg, ...) -- msg can be a stringf or a simple string
+    -- cmdprintf is just printf with the modname prefixed to it
+    printf(cmd_msg_name, stringf(msg, ...)) 
+end
+-- ======================
+-- [[ EXAMPLE UTIL FNS ]]
+-- ======================
 function GetShard()
-    local TheWorld = _G.TheWorld -- TheWorld doesn't exist at compile time, so declare it within the fn
+    local TheWorld = _G.TheWorld -- doesn't exist at compile time, so declare it within the fn
     local shard = "This Shard" -- default if somehow none of the below match
     if TheWorld:HasTag("forest") then
         shard = "Surface"
@@ -64,37 +92,46 @@ function GetShard()
 end
 
 function NoInput(check)
-    local msg = invalid.." Please input a '%s' for the command to work with!"
+    local msg = "Please input a '%s' for the command to work with!"
     if check == "prefab" then
-        msg = stringf(msg, "prefab")
+        check = FirstToUpper(check)
     elseif check == "tendency" then
-        msg = stringf(msg, "Beefalo tendency")
+        check = "Beefalo tendency"
     elseif check == "player" then
-        msg = stringf(msg, "Player number")
+        check = "Player number"
 	else
-		msg = "Seems like I forgot a NoInput check for another kind of variable!"
+		msg = "'%s' is an invalid parameter? Somehow???"
     end
-    print(msg)
+    cmdprintf(msg, check)
+end
+
+function CheckInputType(input, what_type, prefix)
+    prefix = prefix or "Input"
+	local msg = prefix.." '%s' %s!"
+	if not (type(input) == what_type) then
+		local not_type = stringf("is not a %s", what_type)
+		cmdprintf(msg, tostring(input), not_type)
+		return false
+	end
+    return true
 end
 
 function ValidPlayerNum(num)
-	local invalid = prettyname.." Player number '%d'"
-    if not (type(num) == "number") then
-		invalid = gsub(invalid, "%d", "input")
-		invalid = invalid.." is not a number value!"
-        print(invalid)
+    if CheckInputType(num, "number", "Player Number") == false then
         return nil
     elseif num == 0 or (num > #AllPlayers) or (num < 0) then 
-        invalid = invalid.." is outside the current playerlist."
-        printf(invalid, num)
+        local invalid = "Player number '%d' is outside the current playerlist."
+        cmdprintf(invalid, num)
         return nil
     end
     return true
 end
-
+-- ==========================
+-- [[ GIVING INV ITEMS FNS ]]
+-- ==========================
 function DisplayName(prefab)
     local upper = string.upper(prefab) 
-	local invalid = prettyname.." Prefab '%s' has no Display Name!"
+	local invalid = "Prefab '%s' has no Display Name!"
     local name = ""
     for k,v in pairs(STRINGS.NAMES) do
         if k == upper then 
@@ -102,17 +139,17 @@ function DisplayName(prefab)
             return name
         end
     end
-    printf(invalid, prefab)
+    cmdprintf(invalid, prefab)
     name =  "MISSING NAME" 
     return name
 end
 
 function IsInvItem(prefab)
     local prefab = string.lower(prefab)
-	local invalid = prettyname.." Prefab '%s' is not an inventory item!"
+	local invalid = "Prefab '%s' is not an inventory item!"
     local item = SpawnPrefab(prefab) --thanks to Anagram and HarryPPPotter for helping here!
     if item.components.inventoryitem == nil then
-        printf(invalid, prefab)
+        cmdprintf(invalid, prefab)
         item:Remove() -- this is so stupid but whatever
         return nil
     end 
@@ -121,17 +158,15 @@ function IsInvItem(prefab)
 end
 
 function ValidPrefab(prefab)
-	local invalid = prettyname.." Prefab input '%s'"
-    if not (type(prefab) == "string") then
-		prefab = tostring(prefab)
-        printf(invalid.." is not a string!", prefab)
+	local invalid = "Prefab input '%s'"
+    if CheckInputType(prefab, "string", "Prefab") == false then
         return nil
     end
     -- simply using Prefabs = _G.Prefabs at the top returns nil for some reason, 
     -- i thought it gets populated tho? strange
     local lower = string.lower(prefab)
     if _G.Prefabs[lower] == nil then 
-        printf(invalid.." is not an existing prefab!", prefab)
+        cmdprintf(invalid.." is not an existing prefab!", prefab)
         return nil 
     end --thanks to Atoba Azul and thegreatmanagement for the Prefabs[prefab] check line!
     return true
@@ -143,5 +178,9 @@ function GiveInInventory(player, prefab, count)
         player.components.inventory:GiveItem(SpawnPrefab(prefab))
     end
 end
-
--- again, call 'modimport("path_to_util/util")' to use this!
+-- ==================
+-- [[ DEBUG PRINTS ]]
+-- ==================
+debugprint("Here we go!")
+cmd_msg = " Declaring command %s..."
+debugprint(cmd_msg, "c_listcmd and c_helpcmd")
